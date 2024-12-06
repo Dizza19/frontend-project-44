d3c8c5754a0395cd8e01efa504ac2a0c08f02054
root <root@DESKTOP-NSO6VME.>
2024-12-07 02:38:37 +0500

    Исправление5

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,30 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-
+import importPlugin from "eslint-plugin-import"; 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
+  {
+    plugins: {
+      import: importPlugin, 
+    },
+    rules: {
+      "import/no-unresolved": "error",
+      "import/named": "error",
+      "import/namespace": "error",
+      "import/default": "error",
+      "import/export": "error",
+
+      
+      "indent": ["error", 2],
+      "func-style": ["error", "expression", { "allowArrowFunctions": true }],
+      "linebreak-style": ["error", "unix"],
+    },
+  },
 ];
